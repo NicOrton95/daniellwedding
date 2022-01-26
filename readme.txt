e11b291cfc7f333e29d2963c9acfb90f9f9ad9ea
NicOrton95 <94746751+NicOrton95@users.noreply.github.com>
2022-01-26 11:52:48 +0200

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,16 @@
+Final touches:
+
+	When 100% happy with it, upload files to github
+
+
+Fonts Info:
+	Header (Gen & Jason part): Imperial Script, Dancing Script
+	Menu: Dancing Script (look for something a lil less cursive
+		Maybe: Poppins
+
+
+Keep in mind for responsive sizing:
+Small (smaller than 640px) // mobile
+Medium (641px to 1007px) // tablet
+Large (1008px and larger) // desktop
+
---
+++ storage.php
@@ -0,0 +1,199 @@
+<!-- a place to store snippets of code that are cut out in case I want to add them back later -->
+
+
+<!-- Gallery code CSS -->
+
+#gallery-container > img{
+    width: 450px;
+}
+
+
+#gallery-container{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    align-content: center;
+    justify-content: space-evenly;
+
+}
+
+#gallery-container > img{
+    width: 
+}
+
+<!-- Gallery code html -->
+
+<div id="all-gal-img-cont">
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery1-resize.jpg" alt="weddingpic1" id="galleryimg1" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery2-resize.jpg" alt="weddingpic2" id="galleryimg2" width="250">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery3-resize.jpg" alt="weddingpic" id="galleryimg3" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery4-resize.jpg" alt="weddingpic" id="galleryimg4" width="250">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery5-resize.jpg" alt="weddingpic" id="galleryimg5" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery6-resize.jpg" alt="weddingpic" id="galleryimg6" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery7-resize.jpg" alt="weddingpic" id="galleryimg7" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery8-resize.jpg" alt="weddingpic" id="galleryimg8" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                 <img src="images/gallery/wedding-gallery9-resize.jpg" alt="weddingpic" id="galleryimg9" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery10-resize.jpg" alt="weddingpic" id="galleryimg10" width="250"> 
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery11-resize.jpg" alt="weddingpic" id="galleryimg11" width="250">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery12-resize.jpg" alt="weddingpic" id="galleryimg12" width="250">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery13-resize.jpg" alt="weddingpic" id="galleryimg13" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery14-resize.jpg" alt="weddingpic" id="galleryimg14" width="400">
+                </div>
+
+                <div class="gal-img-ind">
+                    <img src="images/gallery/wedding-gallery15-resize.jpg" alt="weddingpic" id="galleryimg15" width="250">
+                </div>
+                        </div>
+            </div>
+
+<div id="start-gal-cont"><h1 class="section-heading gal-heading">Gallery</h1>
+                <div class="general-spacer-small"></div>
+                <div id="gallery-container">
+                <img src="images/gallery/wedding-gallery1-resize.jpg" alt="weddingpic1" id="galleryimg1">
+                <img src="images/gallery/wedding-gallery2-resize.jpg" alt="weddingpic2" id="galleryimg2">
+                <img src="images/gallery/wedding-gallery3-resize.jpg" alt="weddingpic" id="galleryimg3">
+                <img src="images/gallery/wedding-gallery4-resize.jpg" alt="weddingpic" id="galleryimg4">
+                <img src="images/gallery/wedding-gallery5-resize.jpg" alt="weddingpic" id="galleryimg5"> 
+                <img src="images/gallery/wedding-gallery6-resize.jpg" alt="weddingpic" id="galleryimg6">
+                <img src="images/gallery/wedding-gallery7-resize.jpg" alt="weddingpic" id="galleryimg7">
+                <img src="images/gallery/wedding-gallery8-resize.jpg" alt="weddingpic" id="galleryimg8">
+                <img src="images/gallery/wedding-gallery9-resize.jpg" alt="weddingpic" id="galleryimg9">
+                <img src="images/gallery/wedding-gallery10-resize.jpg" alt="weddingpic" id="galleryimg10">
+                <img src="images/gallery/wedding-gallery11-resize.jpg" alt="weddingpic" id="galleryimg11">
+                <img src="images/gallery/wedding-gallery12-resize.jpg" alt="weddingpic" id="galleryimg12">
+                <img src="images/gallery/wedding-gallery13-resize.jpg" alt="weddingpic" id="galleryimg13">
+                <img src="images/gallery/wedding-gallery14-resize.jpg" alt="weddingpic" id="galleryimg14">
+                <img src="images/gallery/wedding-gallery15-resize.jpg" alt="weddingpic" id="galleryimg15">
+                </div>
+            </div>
+
+
+
+
+<!-- Gallery original divs -->
+
+<div id="start-gal-cont"><h1 class="section-heading gal-heading">Gallery</h1></div>
+            <div class="general-spacer-small"></div>
+            <div id="gallery-container">
+                <h1>check storage file for links to images, gonna have to resize & replace it</h1>
+            </div>
+
+
+
+
+
+<!-- stopped working when link was added -->
+.mid-menu-grid{
+    display: flex;
+    justify-content: space-evenly;
+    column-gap: 50px;
+}
+
+
+.mid-menu-grid{
+    font-family: 'Poppins';
+    font-size: 17px;
+} 
+
+
+<!-- Wedding details & invite section with 3 blocks inside container - test -->
+<div id="wedding-details-container2">
+    <div class="wedding-details-three-containers"><h3>blank container</h3></div>
+    <div class="wedding-details-three-containers">actual details to go here</div>
+    <div class="wedding-details-three-containers">blank container</div>
+</div>
+
+
+<!-- This code works for referencing the URL for a background image, for some reason, not working how it worked before when I could just pull the image name:
+background-image: url("http://localhost:4000/desktop/WeddingPortfolioWebsite/images/wedding-slider-img1.png"); -->
+
+
+<!-- Home Page Slider -->
+<div class="slider-img-section-container">
+    <div class="slider-img-grid-flex fade">
+        <img src="images/wedding-slider-img1.png" style="width:100%">
+    </div>
+    <div class="slider-img-grid-flex fade">
+        <img src="images/wedding-slider-img2.png" style="width:100%">
+    </div>
+    <div class="slider-img-grid-flex fade">
+        <img src="images/wedding-slider-img3.png" style="width:100%">
+    </div>
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br/>
+<br/>
+
+<!-- JS code for home page slider  -->
+
+// Slideshow functions
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/* Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+} */
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider-img-grid-flex");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
